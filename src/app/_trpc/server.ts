6de6a61103e9b262a48ac24470c24581582d0d64
riteshreg <riteshregmi85@gmail.com>
2024-07-06 17:31:01 +0545

    updated_url"

--- src/app/_trpc/server.ts
+++ src/app/_trpc/server.ts
@@ -4,7 +4,7 @@ import {  createCaller} from "@/server";
 export const serverClient = createCaller({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/api/trpc",
+      url: "https://trpc-five.vercel.app/api/trpc",
     }),
   ],
 });

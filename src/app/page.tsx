54b2a2c2f4ed3262203565dc9e737ab6b6c07941
riteshreg <riteshregmi85@gmail.com>
2024-07-06 17:27:45 +0545

    removed_next_auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-
 import AddTodo from "./_components/AddTodo";
 import ListTodos from "./_components/ListTodos";
 import { trpc } from "./_trpc/client";
 import { serverClient } from "./_trpc/server";
 
 export default async function Home() {
-
   const hello = await serverClient.allTodos();
-
 
   return (
     <div className="flex items-center pt-4 flex-col">
       <AddTodo />
-      <ListTodos initialTodos={hello}/>
+      <ListTodos initialTodos={hello} />
     </div>
-  )
+  );
 }
